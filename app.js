315048a14229d78d38d6a42bd32df5841aae5e3c
Junaidi Rahmat <junaidirahmat02@gmail.com>
2024-10-09 20:28:56 +0700

    Feat : handlle CORS

--- app.js
+++ app.js
@@ -4,28 +4,34 @@ const blogRoutes = require("./routes/blogRoutes.js");
 const assistantRoutes = require("./routes/assistantRoutes.js");
 const galleryRoutes = require("./routes/galleryRoutes.js");
 const session = require("express-session");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
+
+const app = express();
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true },
+    cookie: { secure: process.env.NODE_ENV === "production" }, // Secure cookie hanya aktif di production
   })
 );
 
 let corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://www.cpslaboratory.com"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true, // Izinkan pengiriman cookie dan credentials
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); // Handle preflight requests
+
 app.use(express.json());
 
 app.use("/auth", adminRoutes);
-app.use("/apiv1", blogRoutes);
-app.use("/apiv2", assistantRoutes);
-app.use("/apiv3", galleryRoutes);
+app.use("/api", blogRoutes);
+app.use("/api", assistantRoutes);
+app.use("/api", galleryRoutes);
 
 module.exports = app;
