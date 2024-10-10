6a84d6f28bb0d449c6944fbf77a93f4951bf724e
Junaidi Rahmat <junaidirahmat02@gmail.com>
2024-10-10 19:45:05 +0700

    feat : 20 mb photo

--- middleware/multer.js
+++ middleware/multer.js
@@ -4,7 +4,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 15 * 1024 * 1024 },
+  limits: { fileSize: 20 * 1024 * 1024 },
 });
 
 module.exports = upload;
